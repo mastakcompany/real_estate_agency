9db069a042fe7a4fb5fd8939394f2f9d6dbaae29
Jora <joradesktop@joradesktop.com>
2023-08-04 17:16:31 +0700

    Added a list of owners to the flat page

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner',)
+    
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ('created_at',)
@@ -11,6 +16,10 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('likes',)
+    
+    inlines = [
+        OwnerInline,
+    ]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
@@ -22,7 +31,7 @@ class OwnerAdmin(admin.ModelAdmin):
     list_display = ('name', 'phonenumber',)
     raw_id_fields = ('flats',)
     
-    
+
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
 admin.site.register(Owner, OwnerAdmin)
